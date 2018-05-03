57c40c0c2494ebd7331a8cd4868a1615db26fa94
Luna Fidalgo <lunafidalgo@gmail.com>
2018-05-03 16:01:52 +0200

    Minor details

--- starter-code/client/src/app/retrieve-journal-entries.service.ts
+++ starter-code/client/src/app/retrieve-journal-entries.service.ts
@@ -1,24 +1,21 @@
 import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
-import { Observable } from "rxjs/Observable";
-import "rxjs/add/operator/map";
+import "rxjs";
 
 @Injectable()
 export class RetrieveJournalEntriesService {
+  BASE_URL: string = "http://localhost:3000/api/";
   constructor(private http: Http) {}
 
   getJournalEntries() {
     return this.http
-      .get("http://localhost:3000/api/journal-entries")
+      .get(`${this.BASE_URL}/journal-entries`)
       .map((res: Response) => res.json());
   }
 
   getJournalEntry(id) {
-
     return this.http
-      .get(`http://localhost:3000/api/journal-entries/${id}`)
+      .get(`${this.BASE_URL}/journal-entries/${id}`)
       .map((res: Response) => res.json());
   }
-
-
 }
